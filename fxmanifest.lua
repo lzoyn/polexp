46927e65fa30173c6f6ba89944a6dcdb6149e7e2
lzoyn <73012464+lzoyn@users.noreply.github.com>
2022-02-19 16:40:08 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'explorercarcols.meta',
+    'handling.meta',
+}
+ 
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'explorercarcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+ 
